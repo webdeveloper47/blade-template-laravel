857ad6ef50e6d90588cb08eff42319e7fd250b5d
miraj <mirajulislam0724@gmail.com>
2023-05-24 18:23:30 +0600

    problem solved

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -3,7 +3,11 @@
 
 
 @section('content')
-
+    @include('Component.hero')
+    @include('Component.howWeDo')
+    @include('Component.pricing')
+    @include('Component.team')
+    @include('Component.newslatter')
 
 
 @endsection
